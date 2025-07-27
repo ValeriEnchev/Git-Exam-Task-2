f32abca866b6efce7ec51177c5af8f80a53bae17
ValeriEnchev <valee70@mail.bg>
2025-07-27 09:50:37 +0300

    TASK-2: initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+
+## To run formatting
+
+npm run format
+
+## To set hooks path
+
+git config core.hooksPath .githooks
---
+++ sum.test.js
@@ -0,0 +1,1 @@
+const sum = require('./sum'); test('adds 1 and 2 result should be equal to 3', () => { expect(sum(1, 2)).toBe(3); });
